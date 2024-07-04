6d9df4eb26f143c1b3d738c197aee64261c05f71
nikitastogniy812 <nikitastognii@gmail.com>
2024-07-04 22:09:32 +0300

    fix blyad

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
 import Header from "@/components/Header";
 import Content from "@/components/Search/Content";
 import SearchSection from "@/components/Search/SearchSection";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-start  bg-black pb-4">
-      <Header />
-      <SearchSection />
-      <Content />
-    </main>
+    <Suspense fallback={<div>Loading...</div>}>
+      <main className="flex flex-col items-center justify-start  bg-black pb-4">
+        <Header />
+        <SearchSection />
+        <Content />
+      </main>
+    </Suspense>
   );
 }
